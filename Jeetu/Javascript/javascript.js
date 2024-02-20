3ef4adb9e0f1deaf7c3baed3198dcdb55bc6e442
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-02-20 10:45:09 +0530

    JS

--- Jeetu/Javascript/javascript.js
+++ Jeetu/Javascript/javascript.js
@@ -1,1 +1,25 @@
-console.log("heloo")
+// single line comments 
+
+/*
+    Javascript is a scripted language interpted and precompiled langugae.
+    which is used in web development.
+
+    link: 
+    1.internal JS ==  <script> </script>
+    2. external JS == <script src ="filename"> </script>
+
+    JAVSCRIPT Extension = .js
+
+*/ 
+console.log("jettu")
+console.error("Error")
+console.warn("warning")
+document.write("Hello")
+// alert("Alert")
+
+// User Input == always return a string 
+
+x = prompt("User Input","Enter Your Name")
+console.log(x)
+
+
