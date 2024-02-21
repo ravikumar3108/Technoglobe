389a558cf6052d43ef57e0e60634336f690c8c3b
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-02-21 10:57:10 +0530

    js

--- Jeetu/Javascript/javascript.js
+++ Jeetu/Javascript/javascript.js
@@ -2,24 +2,91 @@
 
 /*
     Javascript is a scripted language interpted and precompiled langugae.
-    which is used in web development.
+    which is used in web development. case-senstive language
 
     link: 
     1.internal JS ==  <script> </script>
     2. external JS == <script src ="filename"> </script>
 
-    JAVSCRIPT Extension = .js
+//     JAVSCRIPT Extension = .js
 
-*/ 
-console.log("jettu")
-console.error("Error")
-console.warn("warning")
-document.write("Hello")
-// alert("Alert")
+// */ 
+// console.log("jettu")
+// console.error("Error")
+// console.warn("warning")
+// document.write("Hello")
+// // alert("Alert")
 
-// User Input == always return a string 
+// // User Input == always return a string 
 
-x = prompt("User Input","Enter Your Name")
-console.log(x)
+// x = prompt("User Input","Enter Your Name")
+// console.log(x)
+
+
+// // Variables :- variables are used to store our data values 
+
+// // x = variable name 
+// //  = assingment operator 
+// //  10 = data / value
+
+// x = 10 
+
+// // variables name 
+// // 1.alphabhets 
+
+// jeetu = 40
+// console.log(jeetu)
+
+// // 2 underscore _
+
+// _somendar = 400 
+// console.log(_somendar)
+
+// // 3. number + character 
+// dev21 = 30
+
+// // 4. camelCase
+
+// jettuKumar = 20 
+
+
+// keywords = let, var, const 
+
+// 1. let 
+// A. let cannot be re-declared 
+let y = 20 
+console.log(y)
+// let y = 40
+
+//  re-Assign 
+y = 40
+console.log(y)
+
+//  B. let can be a block scope 
+
+{
+    let z = 30
+}
+
+// 2. Var 
+//  A. let can be re-declared 
+//  re-Assign
+// B.let cannot be a block scope 
+
+
+// 3. const 
+//  A. let cannot be re-declared 
+//  B . caanot br re-Assign
+// C.let can be a block scope 
+
+
+//  
+
+
+
+
+
+
+
 
 
