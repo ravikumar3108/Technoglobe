130a097f45ebd41870b5c60a4347ad024d59b88c
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-02-23 10:55:40 +0530

    operators

---
+++ Jeetu/Javascript/Operators.js
@@ -0,0 +1,70 @@
+// 1. Arithmetic Opertaors 
+// 2.Assigment Opertaors
+// 3. Comparision Operators
+// 4. Logical Opertaors 
+
+
+// 1. Arithmetic Opertaors 
+
+    // +  add 
+    // - substract 
+    // * multiply
+    // / divide 
+    // %  modulus = remainder 
+    // ** expontation = power 
+    //  ++ increment 
+    //  -- decremement 
+
+
+
+// let num1 = 30
+// let num2 = 29
+
+// let add = num1+num2
+// console.log(add)
+
+
+// let y = 13
+// let z = 3
+
+// let a = y % z 
+// console.log("Moduls = " , a)
+
+// let p = y ** z
+// console.log(p)
+
+
+// let inc = 10 
+// // prefix 
+
+// let i = ++inc
+// console.log(i)
+
+// //  postfix 
+// let pi = inc++
+// console.log(pi)
+// console.log(inc)
+
+
+// Assignment Operator 
+//   =equal to 
+//  += 
+//  -= 
+//  *=
+//  /= 
+//  %= 
+//  **= 
+
+
+
+
+
+let num1 = 10
+
+let d = num1 += 10 
+console.log("value of d", d)
+console.log("value of num1", num1)
+
+
+
+
