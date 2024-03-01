afaee77ccb8f698e4c5276e97ac1a7e8707db655
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-03-01 11:53:51 +0530

    node

--- Node JS/Priyanshu/index.js
+++ Node JS/Priyanshu/index.js
@@ -1,14 +1,20 @@
 
 const express = require('express')
-
-// 
 const app = express()
 
+app.set('view engine','ejs') 
 // make a server 
+app.use(express.static(__dirname + "/public"))
+
 
 app.get("",(req,res)=>{
-    res.send("hleo priyanshuffewrewrewrewrewrewr")
+    res.send("hleo priyanshu")
 })
+
+app.get("/home",(req,res)=>{
+    res.render("Home")
+})
+
 console.log("hii")
 app.listen(5000,()=>{
     console.log("server crtera")
