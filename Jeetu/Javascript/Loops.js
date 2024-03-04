1afbc7f998c5344da6203ae58583822f89f62c73
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-03-04 11:10:29 +0530

    hfds

---
+++ Jeetu/Javascript/Loops.js
@@ -0,0 +1,40 @@
+//  Loops :- It is a statement which is run as well as our condition is true .
+
+// 1. initialize
+// 2 . Condition
+// '3. Increment / decrement
+
+//  while loop
+//  Do - while loop == expection case one time run when our condition is false 
+// For loop  
+
+//  syntax of while loops 
+
+// while(condition){
+//    CODE 
+//     increment / decrement 
+// }
+
+
+// let x = 1
+
+// while (x > 20) {
+//     console.log(x)
+//     x++
+// }
+
+// let y = 5
+
+// do {
+//     console.log(y)
+//     y++
+// } while (y > 20)
+
+
+
+
+for(let x = 0 ; x < 30 ; x ++){
+        console.log(x)
+}
+
+
