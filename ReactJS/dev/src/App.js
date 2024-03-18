7d7336e8b9a60d97fc2e62cf3f58ae7dcfb021b6
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-03-18 11:49:06 +0530

    react

---
+++ ReactJS/dev/src/App.js
@@ -0,0 +1,21 @@
+// Function Components 
+//  Create a function Components
+// Export the Component
+// Call the function Component
+
+function App() {
+  return (
+    // <></> JSX fragments :- this fragments allow to wriiten html code
+    <>
+      <div>
+        <div>
+          <h1>Helooo</h1>
+        </div>
+      </div>
+      <div></div>
+    </>
+  )
+}
+
+
+export default App;
