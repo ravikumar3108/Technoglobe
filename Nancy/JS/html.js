ce20e156b2e698674489329a353f8d50c7e5b4dc
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-03-29 10:09:56 +0530

    op

---
+++ Nancy/JS/html.js
@@ -0,0 +1,71 @@
+// Operators :- 
+// 1. arithmetic opertaors 
+    // + , - , * , / , 
+    // % = modulus = result(remainder)
+    // ** = expontentaion == power
+    // ++ increment  = +1 
+    // -- decrement = -1 
+
+    let num1  = 32
+    let num2 = 23
+    console.log(num1%num2)
+
+    console.log(num1**2)
+    
+    // prefix 
+    // ++x 
+
+    let x = 20
+    let inc = ++x 
+    console.log("pre",inc)
+    console.log("inc",x)
+    document.write(x,"<br>")
+    document.write(inc)
+
+
+    //  postfix 
+    // x++ 
+
+    let x1 = 20
+    let inc1 = x++
+    console.log("post",inc1)
+    console.log("inc1",x1)
+
+
+
+
+    
+
+
+
+// 2. Assignment operators 
+    //  = 
+    //  += 
+    //  -= 
+    //  *= 
+    //  /=
+    //  %= 
+    //  **=
+
+
+
+
+// 3. comparision operators 
+    // == 
+    // ===
+    // != 
+    // !==
+    // >
+    // <
+    // >=
+    // <= 
+
+
+
+// 4. logical operators 
+    // ! == not 
+    // && == and 
+    // || == or 
+
+
+
