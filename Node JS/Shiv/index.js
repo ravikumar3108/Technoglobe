f8c7b69d37c8e2b678e516e15c129695f69ce9ab
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-01 11:49:11 +0530

    moongoose

--- Node JS/Shiv/index.js
+++ Node JS/Shiv/index.js
@@ -1,15 +1,36 @@
-console.log("heloo")
 
 const express = require("express")
 const cors = require('cors')
+const mongoose = require('mongoose');
+
 
 const app = express()
-
-
 app.use(cors())
-
 app.use(express.json())
 // Api == get , post  
+
+
+
+// getting-started.js
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/shiv');
+  console.log("Database Connect (Shiv)")
+}
+
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true
+    },
+    email:String,
+    password:String,
+  });
+
+  const User = mongoose.model('user', UserSchema);
+
 
 
 app.get("",(req,res)=>{
@@ -20,24 +41,27 @@ app.get("",(req,res)=>{
     })
 })
 
-app.post("",(req,res)=>{
+app.post("/register", async(req,res)=>{
+    console.log(req.body)
+    const profile = new User({
+        username : req.body.name,
+        email : req.body.email,
+        password : req.body.password,
+    })
+    await profile.save()
     res.json({
         method:"post",
-        message:"Successful",
-        author:"shiv",
-        bloodgp : "A+"
+        data:req.body
     })
     console.log(req.body)
 })
 
-app.post("/register",(req,res)=>{
-    console.log(req.body)
-    res.json({
-        message:"aa gya"
-    })
-})
-
-
+// app.post("/register",(req,res)=>{
+//     console.log(req.body)
+//     res.json({
+//         message:"aa gya"
+//     })
+// })
 
 app.listen(3030,()=>{
     console.log("Server Start on Port 3030")
