e738d7beacd96ee9e544ca798a92802fa2facc60
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-02 12:00:34 +0530

    user

--- Node JS/Shiv/index.js
+++ Node JS/Shiv/index.js
@@ -15,55 +15,66 @@ app.use(express.json())
 
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/shiv');
-  console.log("Database Connect (Shiv)")
+    await mongoose.connect('mongodb://127.0.0.1:27017/shiv');
+    console.log("Database Connect (Shiv)")
 }
 
 
 const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        require:true
+    username: {
+        type: String,
+        require: true
     },
-    email:String,
-    password:String,
-  });
+    email: String,
+    password: String,
+});
 
-  const User = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
 
 
 
-app.get("",(req,res)=>{
+app.get("", (req, res) => {
     res.json({
-        message:"Successful",
-        author:"shiv",
-        bloodgp : "A+"
+        message: "Successful",
+        author: "shiv",
+        bloodgp: "A+"
     })
 })
 
-app.post("/register", async(req,res)=>{
+app.post("/register", async (req, res) => {
     console.log(req.body)
     const profile = new User({
-        username : req.body.name,
-        email : req.body.email,
-        password : req.body.password,
+        username: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
     })
     await profile.save()
     res.json({
-        method:"post",
-        data:req.body
+        method: "post",
+        data: req.body
     })
     console.log(req.body)
 })
+app.post("/login", async (req, res) => {
+    // console.log(req.body)
+    const { email, password } = req.body
 
-// app.post("/register",(req,res)=>{
-//     console.log(req.body)
-//     res.json({
-//         message:"aa gya"
-//     })
-// })
+    const getuser = await User.findOne({ email })
+    console.log(getuser.password)
+    try {
+        if (getuser && getuser.password == password) {
+            res.json({
+                message: "Succesfull"
+            })
+        }
+    } catch (error) {
+      console.log(error)
+    }
 
-app.listen(3030,()=>{
+})
+
+
+app.listen(3030, () => {
     console.log("Server Start on Port 3030")
 })
 
