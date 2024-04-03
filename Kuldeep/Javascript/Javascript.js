3f35eef2769230781a3456bb92af729a7109a87e
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-03 13:28:52 +0530

    js

---
+++ Kuldeep/Javascript/Javascript.js
@@ -0,0 +1,13 @@
+//  Output property 
+console.log("Kuldeep")
+console.warn("Warning")
+console.error("Error")
+
+document.write("Heloo")
+
+alert("Warning")
+
+//  User Input 
+x = prompt("Name","Enter Yoour Name")
+console.log(x) 
+
