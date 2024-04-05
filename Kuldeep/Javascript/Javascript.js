8ee06dc4f82011c2c14ef41e432f6e26fe036c02
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-05 13:54:53 +0530

    variables

--- Kuldeep/Javascript/Javascript.js
+++ Kuldeep/Javascript/Javascript.js
@@ -1,13 +1,74 @@
-//  Output property 
-console.log("Kuldeep")
-console.warn("Warning")
-console.error("Error")
+// //  Output property 
+// console.log("Kuldeep")
+// console.warn("Warning")
+// console.error("Error")
 
-document.write("Heloo")
+// document.write("Heloo")
 
-alert("Warning")
+// alert("Warning")
 
-//  User Input 
-x = prompt("Name","Enter Yoour Name")
-console.log(x) 
+// //  User Input 
+// x = prompt("Name", "Enter Yoour Name")
+// console.log(x)
+
+// Variables :- Variables are used to store our data values 
+
+// left side = variable name 
+// right side = values / data 
+// = => assignment opertaors
+
+x = 30
+
+//  Variables names 
+// Alphabets =  kuldeep , x , y , 
+// Alpha + num = kuldepp23 
+// underscore = _x , _kuldeep
+// camelcase = kuldeepSinghKumar
+//  case-sensitive = x , X both x variab;le is different 
+
+// keywords :-
+// let 
+// 1. let cannot be re-declared
+// 2. let can be a block scope
+
+let y = 20
+// let y = 30
+console.log(y)
+
+// re-assign the value of x 
+y = 40
+console.log(y)
+
+{
+    let z = 20
+    console.log("z value is",z)
+}
+
+
+
+
+
+// // var = variables
+// 1. var can be re-declared
+// 2. var cannot be a block scope
+
+var v = 30
+
+var v = 50
+console.log(v)
+
+
+
+// // const = constant  
+// 1. const cannot be re-declared
+// 2. const can be a block scope
+// 3. const cannot be re-asssign
+
+const con = 30
+// con = 40
+console.log(con)
+
+
+
+
 
