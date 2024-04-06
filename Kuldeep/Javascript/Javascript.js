349980e33b9d049bcc13e3c026662e7245eee7f1
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-06 14:24:16 +0530

    data types

--- Kuldeep/Javascript/Javascript.js
+++ Kuldeep/Javascript/Javascript.js
@@ -1,74 +1,119 @@
-// //  Output property 
-// console.log("Kuldeep")
-// console.warn("Warning")
-// console.error("Error")
+// // //  Output property
+// // console.log("Kuldeep")
+// // console.warn("Warning")
+// // console.error("Error")
 
-// document.write("Heloo")
+// // document.write("Heloo")
 
-// alert("Warning")
+// // alert("Warning")
 
-// //  User Input 
-// x = prompt("Name", "Enter Yoour Name")
-// console.log(x)
+// // //  User Input
+// // x = prompt("Name", "Enter Yoour Name")
+// // console.log(x)
 
-// Variables :- Variables are used to store our data values 
+// // Variables :- Variables are used to store our data values
 
-// left side = variable name 
-// right side = values / data 
-// = => assignment opertaors
+// // left side = variable name
+// // right side = values / data
+// // = => assignment opertaors
 
-x = 30
+// x = 30
 
-//  Variables names 
-// Alphabets =  kuldeep , x , y , 
-// Alpha + num = kuldepp23 
-// underscore = _x , _kuldeep
-// camelcase = kuldeepSinghKumar
-//  case-sensitive = x , X both x variab;le is different 
+// //  Variables names
+// // Alphabets =  kuldeep , x , y ,
+// // Alpha + num = kuldepp23
+// // underscore = _x , _kuldeep
+// // camelcase = kuldeepSinghKumar
+// //  case-sensitive = x , X both x variab;le is different
 
-// keywords :-
-// let 
-// 1. let cannot be re-declared
-// 2. let can be a block scope
+// // keywords :-
+// // let
+// // 1. let cannot be re-declared
+// // 2. let can be a block scope
 
-let y = 20
-// let y = 30
-console.log(y)
+// let y = 20
+// // let y = 30
+// console.log(y)
 
-// re-assign the value of x 
-y = 40
-console.log(y)
+// // re-assign the value of x
+// y = 40
+// console.log(y)
 
-{
-    let z = 20
-    console.log("z value is",z)
+// {
+//     let z = 20
+//     console.log("z value is",z)
+// }
+
+
+
+
+
+// // // var = variables
+// // 1. var can be re-declared
+// // 2. var cannot be a block scope
+
+// var v = 30
+
+// var v = 50
+// console.log(v)
+
+
+
+// // // const = constant
+// // 1. const cannot be re-declared
+// // 2. const can be a block scope
+// // 3. const cannot be re-asssign
+
+// const con = 30
+// // con = 40
+// console.log(con)
+
+
+//  DataTypes: -
+// 1. Number
+// 2 . String == "" , '' String are used to store our character values 
+// 3. null  
+//  4. undefined 
+// 5 . bollean = true and false 
+// 6. array =  Array is used to store multiple values 
+
+let arr = [1,35,46,65,768,"hgadfha", true , null, ]
+console.log(arr)
+
+// 7. Object = Object is used to store multiple values but in form of key and value
+let obj = {
+    name1 : "Kulddeep",
+    clase : "12th",
+    rollno : 324
+}
+let obj1 = {
+    name1 : "Kulddeep1",
+    clase : "12th",
+    rollno : 324
+}
+let obj2 = {
+    name1 : "Kulddeep2",
+    clase : "12th",
+    rollno : 324
 }
 
+console.log(obj.name1)
+console.log(obj1.name1)
 
+console.log(obj)
 
+let x = 30
 
+let name2 = "kuldeep"
+console.log(name2)
+let name1 = 'kuldeep'
+console.log(name1)
 
-// // var = variables
-// 1. var can be re-declared
-// 2. var cannot be a block scope
+let y ;
+console.log(y)
 
-var v = 30
+let z = null 
+console.log(z)
 
-var v = 50
-console.log(v)
-
-
-
-// // const = constant  
-// 1. const cannot be re-declared
-// 2. const can be a block scope
-// 3. const cannot be re-asssign
-
-const con = 30
-// con = 40
-console.log(con)
-
-
-
-
+let bl = true 
 
