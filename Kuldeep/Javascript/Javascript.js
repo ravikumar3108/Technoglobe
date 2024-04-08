d4362dcad7eac46e9dc29697fa1b3d0fb77df208
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-08 14:30:03 +0530

    op

--- Kuldeep/Javascript/Javascript.js
+++ Kuldeep/Javascript/Javascript.js
@@ -1,119 +1,240 @@
-// // //  Output property
-// // console.log("Kuldeep")
-// // console.warn("Warning")
-// // console.error("Error")
+// // // //  Output property
+// // // console.log("Kuldeep")
+// // // console.warn("Warning")
+// // // console.error("Error")
 
-// // document.write("Heloo")
+// // // document.write("Heloo")
 
-// // alert("Warning")
+// // // alert("Warning")
 
-// // //  User Input
-// // x = prompt("Name", "Enter Yoour Name")
-// // console.log(x)
+// // // //  User Input
+// // // x = prompt("Name", "Enter Yoour Name")
+// // // console.log(x)
 
-// // Variables :- Variables are used to store our data values
+// // // Variables :- Variables are used to store our data values
 
-// // left side = variable name
-// // right side = values / data
-// // = => assignment opertaors
+// // // left side = variable name
+// // // right side = values / data
+// // // = => assignment opertaors
 
-// x = 30
+// // x = 30
 
-// //  Variables names
-// // Alphabets =  kuldeep , x , y ,
-// // Alpha + num = kuldepp23
-// // underscore = _x , _kuldeep
-// // camelcase = kuldeepSinghKumar
-// //  case-sensitive = x , X both x variab;le is different
+// // //  Variables names
+// // // Alphabets =  kuldeep , x , y ,
+// // // Alpha + num = kuldepp23
+// // // underscore = _x , _kuldeep
+// // // camelcase = kuldeepSinghKumar
+// // //  case-sensitive = x , X both x variab;le is different
 
-// // keywords :-
-// // let
-// // 1. let cannot be re-declared
-// // 2. let can be a block scope
+// // // keywords :-
+// // // let
+// // // 1. let cannot be re-declared
+// // // 2. let can be a block scope
 
-// let y = 20
-// // let y = 30
-// console.log(y)
+// // let y = 20
+// // // let y = 30
+// // console.log(y)
 
-// // re-assign the value of x
-// y = 40
-// console.log(y)
+// // // re-assign the value of x
+// // y = 40
+// // console.log(y)
 
-// {
-//     let z = 20
-//     console.log("z value is",z)
+// // {
+// //     let z = 20
+// //     console.log("z value is",z)
+// // }
+
+
+
+
+
+// // // // var = variables
+// // // 1. var can be re-declared
+// // // 2. var cannot be a block scope
+
+// // var v = 30
+
+// // var v = 50
+// // console.log(v)
+
+
+
+// // // // const = constant
+// // // 1. const cannot be re-declared
+// // // 2. const can be a block scope
+// // // 3. const cannot be re-asssign
+
+// // const con = 30
+// // // con = 40
+// // console.log(con)
+
+
+// //  DataTypes: -
+// // 1. Number
+// // 2 . String == "" , '' String are used to store our character values 
+// // 3. null  
+// //  4. undefined 
+// // 5 . bollean = true and false 
+// // 6. array =  Array is used to store multiple values 
+
+// let arr = [1,35,46,65,768,"hgadfha", true , null, ]
+// console.log(arr)
+
+// // 7. Object = Object is used to store multiple values but in form of key and value
+// let obj = {
+//     name1 : "Kulddeep",
+//     clase : "12th",
+//     rollno : 324
+// }
+// let obj1 = {
+//     name1 : "Kulddeep1",
+//     clase : "12th",
+//     rollno : 324
+// }
+// let obj2 = {
+//     name1 : "Kulddeep2",
+//     clase : "12th",
+//     rollno : 324
 // }
 
+// console.log(obj.name1)
+// console.log(obj1.name1)
+
+// console.log(obj)
+
+// let x = 30
+
+// let name2 = "kuldeep"
+// console.log(name2)
+// let name1 = 'kuldeep'
+// console.log(name1)
+
+// let y ;
+// console.log(y)
+
+// let z = null 
+// console.log(z)
+
+// let bl = true 
 
 
 
+// Operators :- 
+// 1. arithmetic opertaors 
+// + ,
+//  - , 
+// * , 
+// / , 
+// % = modulus = result(remainder)
+// ** = expontentaion == power
+// ++ increment  = +1 
+// -- decrement = -1 
 
-// // // var = variables
-// // 1. var can be re-declared
-// // 2. var cannot be a block scope
+let num1 = 32
+let num2 = 3
+console.log(num1 + num2)
 
-// var v = 30
-
-// var v = 50
-// console.log(v)
-
-
-
-// // // const = constant
-// // 1. const cannot be re-declared
-// // 2. const can be a block scope
-// // 3. const cannot be re-asssign
-
-// const con = 30
-// // con = 40
-// console.log(con)
-
-
-//  DataTypes: -
-// 1. Number
-// 2 . String == "" , '' String are used to store our character values 
-// 3. null  
-//  4. undefined 
-// 5 . bollean = true and false 
-// 6. array =  Array is used to store multiple values 
-
-let arr = [1,35,46,65,768,"hgadfha", true , null, ]
-console.log(arr)
-
-// 7. Object = Object is used to store multiple values but in form of key and value
-let obj = {
-    name1 : "Kulddeep",
-    clase : "12th",
-    rollno : 324
-}
-let obj1 = {
-    name1 : "Kulddeep1",
-    clase : "12th",
-    rollno : 324
-}
-let obj2 = {
-    name1 : "Kulddeep2",
-    clase : "12th",
-    rollno : 324
-}
-
-console.log(obj.name1)
-console.log(obj1.name1)
-
-console.log(obj)
-
-let x = 30
-
-let name2 = "kuldeep"
-console.log(name2)
-let name1 = 'kuldeep'
-console.log(name1)
-
-let y ;
-console.log(y)
-
-let z = null 
+let z = num1 % num2
 console.log(z)
 
-let bl = true 
+console.log(num1 ** 5)
+
+// // prefix 
+// // ++x 
+
+let x = 20
+let inc = ++x
+console.log("inc", inc)
+console.log("x", x)
+
+
+// //  postfix 
+// // x++ 
+let x1 = 20
+let inc1 = x1++
+console.log("inc1", inc1)
+console.log("x1", x1)
+
+
+
+
+
+
+
+
+// 2. Assignment operators 
+//  = 
+//  += 
+//  -= 
+//  *= 
+//  /=
+//  %= 
+//  **=
+
+let asign = 2
+
+let add = asign += 10
+console.log(asign)
+
+
+
+
+
+
+
+// 3. comparision operators = (true and false) works on lopps and conditions 
+// ==  check value
+// === check value and type
+// !=  vice-versa value
+// !== vice-versa value and type
+// >  gretaer than
+// < less than
+// >= greater equal
+// <= less equal
+
+
+
+
+
+let c1 = 30
+let c2 = 30
+
+console.log(c1 >= c2)
+
+
+
+
+// 4. logical operators
+// ! ==> not
+// && ==> and
+// || ==> or
+
+
+
+// and
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+
+// or
+// true && true = true
+// true && false = true
+// false && true = true
+// false && false = false
+
+
+
+let comp1 = 23
+let comp2 = 45
+let comp3 = 23
+let comp4 = "45"
+
+
+let and = (comp1 == comp3) && !(comp2 === comp4)
+console.log(and)
+
+
+
+
 
