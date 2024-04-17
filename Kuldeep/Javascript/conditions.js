39064881be86cf7004c3396bfba1aa0057d743eb
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-17 14:21:08 +0530

    conditions

---
+++ Kuldeep/Javascript/conditions.js
@@ -0,0 +1,41 @@
+// conditions:- it is a statement which is run when our condition is true
+
+
+
+//  Syntax of Condition 
+// if(condition){
+
+// }
+// else if(condition){
+
+// }
+//  default statement
+// else{
+
+// }
+
+
+let x  = 10
+
+if( x == 10){
+    console.log("constion is true")
+}else{
+    alert("Something Wrong")
+}
+
+
+let y = 20
+
+if( y == 11){
+    console.log("y is equal to 11")
+}
+else if( y < 10){
+    console.log("y is greater than 10")
+}
+else if( y == 20){
+    console.log("y is smaller than 10")
+}
+else{
+    alert("Something Wrong")
+}
+
