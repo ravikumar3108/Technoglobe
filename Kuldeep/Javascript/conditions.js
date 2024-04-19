bd1c65e174294540244c08442ad1d88ea8354d1f
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-19 14:38:37 +0530

    loops

--- Kuldeep/Javascript/conditions.js
+++ Kuldeep/Javascript/conditions.js
@@ -15,27 +15,82 @@
 // }
 
 
-let x  = 10
+// let x = 10
 
-if( x == 10){
-    console.log("constion is true")
-}else{
-    alert("Something Wrong")
-}
+// if (x == 10) {
+//     console.log("constion is true")
+// } else {
+//     alert("Something Wrong")
+// }
 
 
-let y = 20
+// let y = 20
 
-if( y == 11){
-    console.log("y is equal to 11")
-}
-else if( y < 10){
-    console.log("y is greater than 10")
-}
-else if( y == 20){
-    console.log("y is smaller than 10")
-}
-else{
-    alert("Something Wrong")
-}
+// if (y == 11) {
+//     console.log("y is equal to 11")
+// }
+// else if (y < 10) {
+//     console.log("y is greater than 10")
+// }
+// else if (y == 20) {
+//     console.log("y is smaller than 10")
+// }
+// else {
+//     alert("Something Wrong")
+// }
+
+
+// // Switch Statement :- 
+
+// let st = 6
+
+// switch (st) {
+//     case 1:
+//         console.log("Case 1")
+//         break;
+//     case 2:
+//         console.log("Case 2")
+//         break;
+//     case 3:
+//         console.log("Case 3")
+//         break;
+//     case 4:
+//         console.log("Case 4")
+//         break;
+//     default:
+//         console.log("default")
+// }
+
+
+//  Loops :- it is a statement which is run when our condition as well as true .
+//  while loop 
+//  do-while 
+//  for loop 
+
+// 1. initialise 
+// 2 . Condtion 
+// 3. increment / decremeent
+
+
+// let x = 1
+
+// while (x >= 20) {
+//     console.log(x)
+//     x++
+// }
+
+
+//  One time run as well as your condtion is false 
+// do {
+//     console.log(x)
+//     x++
+// } while (x > 10)
+
+
+
+//  for loop:- 
+// for (let y = 0; y < 20; y++) {
+//     console.log(y)
+// }
+
 
