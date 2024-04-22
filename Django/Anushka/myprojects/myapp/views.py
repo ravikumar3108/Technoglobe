164501286ecccb4ad67870a23f681d0d3f7fbe12
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-04-22 15:51:54 +0530

    html

--- Django/Anushka/myprojects/myapp/views.py
+++ Django/Anushka/myprojects/myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def Home(request):
+    return HttpResponse("Hii My name is Anushka")
+
+
+def About(request):
+    #  first parameter = req
+    #  second parameter = html file
+    #  third parameter = data
+    return render(request,"index.html",{})
+
+
+
