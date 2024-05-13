5bc0dcd79b333ef9fb0cf61ac13892703d1856c1
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-13 09:49:08 +0530

    js

---
+++ Nishant/Javascript/Javascrip[t.js
@@ -0,0 +1,17 @@
+//  Javascript :-   Javascript is a programing language which is used to develop our web applications
+// Javascript are two types 
+    // 1. Internal Js :- By Script Tag 
+    // 2. External Js :- By external File with extension .js 
+
+//  Javascript is a interpted , precompiled  and Scripted languguage.
+
+console.log("External file")
+
+console.warn("Warning")
+
+console.error("Error") 
+
+document.write("Writeen on web page")
+
+alert("Alert")
+
