9692fc0df642e17915e0ea9b997bf7ce9a46074a
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-15 10:00:11 +0530

    op

--- Nishant/Javascript/Javascrip[t.js
+++ Nishant/Javascript/Javascrip[t.js
@@ -1,17 +1,173 @@
-//  Javascript :-   Javascript is a programing language which is used to develop our web applications
-// Javascript are two types 
-    // 1. Internal Js :- By Script Tag 
-    // 2. External Js :- By external File with extension .js 
+// // // //  Javascript :-   Javascript is a programing language which is used to develop our web applications
+// // // // Javascript are two types 
+// // //     // 1. Internal Js :- By Script Tag 
+// // //     // 2. External Js :- By external File with extension .js 
 
-//  Javascript is a interpted , precompiled  and Scripted languguage.
+// // // //  Javascript is a interpted , precompiled  and Scripted languguage.
 
-console.log("External file")
+// // // console.log("External file")
 
-console.warn("Warning")
+// // // console.warn("Warning")
 
-console.error("Error") 
+// // // console.error("Error") 
 
-document.write("Writeen on web page")
+// // // document.write("Writeen on web page")
 
-alert("Alert")
+// // alert("Alert")
+
+// // //  Variables = variables are used to store our values 
+// // // 1. _Underscore 
+// // // 2. camelCase 
+// // //  3. Alphabets 
+// // // 4 . alphabets + numbers 
+// // // 5._Underscore +  alphabets + numbers 
+
+// // //  declaration of variables 
+
+// // x  =  10 
+
+// // console.log(x)
+
+
+// // //  Keywords 
+// // // let , var , const 
+
+// // // 1. let 
+// // //    let can not be redeclared  
+// // //  let can be a block scope 
+
+
+// // let  y = 30
+// //  y = 80
+// //  console.log(y)
+
+// //  {
+// //     let z = 30
+// //     console.log(z)
+// //  }
+
+
+// //  // 1. var 
+// // //    var can  be redeclared  
+// // //  var cannot be a block scope 
+
+
+// // // const 
+// //     //  connst can not be redeclared 
+// //     // const cannot be reassign 
+
+// //     const cn = 20
+// //     //  cn = 20
+// //     console.log(cn)
+
+
+// // Data Types 
+
+// // Integer / number 
+// // String = String are used to store our character value double quotes / single quotes 
+
+// // null = 
+// // undefined = 
+// // bollean  = true and false 
+// // Array = used to store multiple values 
+
+// // Object  =  used to store multiple values in key and value form 
+
+// let str = "this is my string"
+// let str1 = "this is my string"
+// let str2 = "this is my string"
+// let str3 = "this is my string"
+// let str4 = "this is my string"
+// let str5 = 'this is my string'
+// console.log("str :- ", str)
+
+// let x = null
+// console.log(x)
+
+// //  Array 
+
+// let arr = [21, 3, 5, 333, 3]
+// console.log(arr)
+
+
+// let obj = {
+//    name: "nishant",
+//    classe: "12th"
+// }
+
+// console.log(obj.name)
+
+
+
+// //  OPerators 
+// // 1. Arithmetic Operators
+// // + 
+// // -
+// // *
+// // /
+// // ** expontation == power 
+// // % modulus === remainder 
+// // ++ increment 
+// // -- decrement 
+
+
+// let num1 = 20
+// let num2 = 6
+
+// console.log(num1 ** num2)
+// console.log(num1 % num2)
+
+
+// //  Assignment operators 
+// // =
+// // +=
+// // -+
+// // *=
+// // /=
+
+// let num3 = 20
+
+// num3 += 20
+
+// let n = num3 + 20
+
+
+// console.log(num3)
+
+// console.log(n)
+
+// //  Comaparision op  = bollean value 
+// // ==  check value 
+// //  === check value and type
+// //  != vice versa  ==
+// //  !==  vice versa  ===
+// //  > 
+// //  <
+// // >=
+// //  <= 
+
+let x = 20
+let y = "20"
+let z = 20
+
+console.log(x !== y)
+console.log(x === z)
+
+//  Logical op
+//  and (&&)
+// true && true = true
+// true && fasle = false
+// false && true = false
+// false && false = false
+
+
+// or (||)
+// true && true = true
+// true && fasle = true
+// false && true = true
+// false && false = false
+
+
+//  ! not  
+
 
