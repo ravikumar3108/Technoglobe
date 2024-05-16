9e2d9a9109621565e853e2a96379f2c2eae2eea7
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-16 09:54:09 +0530

    statement

---
+++ Nishant/Javascript/Constions.js
@@ -0,0 +1,58 @@
+
+// if else Statement ;- it is a statement which is run when our condition is true 
+
+// if single condition 
+// else default condition 
+//  else if multiple conditions 
+
+
+// if(condition){
+//     // code 
+// }
+
+let x = 10
+
+if (x == 11) {
+    console.log("X is equal to 11")
+}
+else if (x === 10) {
+    console.log(' x is equal to 10')
+}
+else if (x === 12) {
+    console.log(' x is equal to 12')
+}
+else if (x === 13) {
+    console.log(' x is equal to 13')
+}
+else if (x === 14) {
+    console.log(' x is equal to 14')
+}
+else {
+    console.log("Default conditon")
+}
+
+// switch Statement 
+
+
+let y = 2
+
+switch (y) {
+    case 1:
+        console.log("case 1")
+        break;
+    case 2:
+        console.log("case 2")
+        break;
+    case 3:
+        console.log("case 3")
+        break;
+    case 4:
+        console.log("case 4")
+        break;
+    default:
+        console.log("deafuelt")
+
+}
+
+
+
