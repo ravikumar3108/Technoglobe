d0bd7a76cfc9fb8d82e26804c8198ded216f188e
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-20 11:30:06 +0530

    api

--- ReactJS/jignesh/src/components/Api.jsx
+++ ReactJS/jignesh/src/components/Api.jsx
@@ -1,21 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { porducts } from "./Data";
 
 function Api() {
   //  arrow function ()=>{}
 
-  const Arowfun = () => {};
+  // const Arowfun = () => {};
 
-  return <div>{porducts.map((item) => {
-    return(
-        <>
-        {item.id} <br />
-        {item.title} <br />
-        {item.price} <br />
-        <img src={item.imgae} alt="" />
-        </>
-    ) 
-  })}</div>;
+  // return <div>{porducts.map((item) => {
+  //   return(
+  //       <>
+  //       {item.id} <br />
+  //       {item.title} <br />
+  //       {item.price} <br />
+  //       <img src={item.imgae} alt="" />
+  //       </>
+  //   ) 
+  // })}</div>;
+
+  let [data,setData] = useState([])
+
+  console.log(data)
+
+
+  //  UseEffect : -  side effect of our components
+   async function getData(){
+    let api = await fetch("https://fakestoreapi.com/products")
+    let apijson = await api.json()
+    setData(apijson)
+  }
+  
+  useEffect(()=>{
+    getData()
+  },[])
+
+
+  return(
+    <>
+    
+    
+
+    </>
+  )
+
+
+
 }
 
 export default Api;
