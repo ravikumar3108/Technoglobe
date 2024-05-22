bbdf12be043184fa41b90ed7bd923ec87837c4e8
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-22 10:58:52 +0530

    mongoose

--- Node JS/Vivek/index.js
+++ Node JS/Vivek/index.js
@@ -1,23 +1,42 @@
 const express = require('express')
-
 //  create a  object 
 const app = express()
 const bodyParser = require('body-parser')
 
+const mongoose = require('mongoose');
 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/jeetu');
+}
+
+// Create a Schema (Model)
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    email:String,
+    password:String
+  });
+
+  const users = mongoose.model('users', userSchema);
+
+
+
+
+app.use(express.json())
 
 // parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }))
 // // parse application/json
 // app.use(bodyParser.json())
 
-app.use(express.json())
 
 
 // app.set("view engine" , "ejs")
 // console.log("Nodeon running ")
 //  Api's 
-//  get , post , delete , put 
+//  get ,post , delete , put 
 
 app.get("", (req, res) => {
     // res.render("Home.ejs") 
@@ -27,6 +46,10 @@ app.get("", (req, res) => {
 app.post("", (req, res) => {
     console.log(req.body)
 })
+
+
+
+
 
 
 app.listen(8000, () => {
