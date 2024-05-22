5f2014580130566d64cc5b4030f8931e3220b4fc
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-22 09:59:48 +0530

    react

---
+++ ReactJS/nishant/src/App.js
@@ -0,0 +1,40 @@
+//  Create a function component 
+// export our components
+// import our function components
+// Call the function component 
+
+import Home from "./components/Home"
+
+
+function App(){
+  return(
+    //  JSX fragemnts ;- 
+    <>
+      <div>
+        <h1>React Page</h1>
+        hjdsajdsdgjsdsadsdsd
+        pass
+
+
+        {/*  Call the function component */}
+
+        <Home/>
+        <Home/>
+        <Home/>
+        <Home/>
+        <Home/>
+        <Home/>
+        <Home/>
+
+      </div>
+    </>
+   
+  )
+}
+
+export default App
+
+
+ 
+
+
---
+++ ReactJS/nishant/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1>Nishant</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        molestias ipsam, quidem necessitatibus dolores minus optio adipisci!
+        Dolor similique maiores, qui laboriosam eveniet incidunt cupiditate eos
+        perspiciatis nostrum est maxime?
+      </p>
+    </>
+  );
+}
+
+export default Home;
