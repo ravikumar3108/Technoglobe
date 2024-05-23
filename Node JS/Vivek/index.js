71fa15d248b0212d1372d8e9b757711e33d6bd87
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-05-23 11:08:00 +0530

    mongo

--- Node JS/Vivek/index.js
+++ Node JS/Vivek/index.js
@@ -10,21 +10,31 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/jeetu');
 }
+app.use(express.json())
 
 // Create a Schema (Model)
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    email:String,
-    password:String
-  });
+  username: String,
+  email: String,
+  password: String
+});
 
-  const users = mongoose.model('users', userSchema);
+const users = mongoose.model('jeetukusers', userSchema);
 
+app.post("", (req, res) => {
+  const { username, email, password } = req.body
+  const newuser = new users({
+    username,
+    email,
+    password,
+  })
+  newuser.save()
+  res.json({
+    user: req.body
+  })
+})
 
-
-
-app.use(express.json())
 
 // parse application/x-www-form-urlencoded
 // app.use(bodyParser.urlencoded({ extended: false }))
@@ -39,21 +49,13 @@ app.use(express.json())
 //  get ,post , delete , put 
 
 app.get("", (req, res) => {
-    // res.render("Home.ejs") 
-    res.send("Message")
+  // res.render("Home.ejs") 
+  res.send("Message")
 })
-
-app.post("", (req, res) => {
-    console.log(req.body)
-})
-
-
-
-
 
 
 app.listen(8000, () => {
-    console.log("Server is Created")
+  console.log("Server is Created")
 })
 
 
