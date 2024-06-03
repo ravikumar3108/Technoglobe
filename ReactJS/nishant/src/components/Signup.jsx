73a7c4c89bc07c7919f9ce93d25f891ee1a7a615
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-06-03 10:03:31 +0530

    login

--- ReactJS/nishant/src/components/Signup.jsx
+++ ReactJS/nishant/src/components/Signup.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Signup() {
+  //  Single State Data
+  const [data, setData] = useState("");
+  // const [data, setData] = useState("");
+  // const [data, setData] = useState("");
+  // console.log(data);
+
+  function getData(e) {
+    setData(e.target.value);
+  }
+
+  //  Multiple State Data
+
+  const [allData, setAllData] = useState("");
+  console.log(allData);
+
+  function getData1(e) {
+    setAllData({
+      // Spread operator 
+      ...allData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
   return (
     <div>
       <form action="">
+        <h1>Signup</h1>
         <label htmlFor="">UserName</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="username" id="" onChange={getData1} />
         <label htmlFor="">Email</label>
-        <input type="text" name="" id="" />
+        <input type="text" name="email" id="" onChange={getData1} />
         <label htmlFor="">Password</label>
-        <input type="text" name="" id="" />
-        <button type='submit'>Submit</button>
+        <input type="text" name="password" id="" onChange={getData1} />
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
