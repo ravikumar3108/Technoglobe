457b7604f4dfac32ba6a3646d837f15c2185d40f
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-06-06 12:55:50 +0530

    clas2

--- Node JS/jeetu/index.js
+++ Node JS/jeetu/index.js
@@ -1,20 +1,30 @@
 const express = require("express")
 // create a object 
 const app = express()
-
 //  create a APIs
 
-// get , post , delete , put 
 
+// middlewares
+app.use(express.json())
+
+
+// get , post , delete , put 
 app.get("", (req, res) => {
-    res.send("helkoo")
+    // res.send("helkoo")
     res.json({
         mesage: "sucessfull",
         status: true
     })
-    res.status(400).json({ mesage: "sucessfull", })
+    // res.status(400).json({ mesage: "sucessfull", })
 })
 
+app.post("", (req, res) => {
+    console.log(req.body)
+    res.json({
+        mesage: "post request",
+        status: true
+    })
+})
 
 
 //  create a route 
