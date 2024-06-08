925e96d96cb3ca8e809dd8229811799ec90c2dd3
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-06-08 09:58:41 +0530

    Apii

--- ReactJS/nishant/src/components/Api.jsx
+++ ReactJS/nishant/src/components/Api.jsx
@@ -1,32 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Api() {
+  const [Api, setApi] = useState([]);
+  console.log(Api);
+  async function handleApi() {
+    let data = await fetch("https://fakestoreapi.com/products");
+    let jsondata = await data.json();
+    setApi(jsondata);
+  }
 
-    const [Api,setApi] = useState("")
-    console.log(Api)
-   async function handleApi(){
-        let data = await fetch("https://fakestoreapi.com/products")
-        let jsondata = await data.json()
-        console.log(jsondata)
-        setApi(jsondata)
-    }
+  // useEffect :- Side effect of our component
 
-    
-    // useEffect :- Side effect of our component 
+  useEffect(() => {
+    handleApi();
+  }, []);
 
-useEffect(()=>{
-    handleApi()
-},[])
+  // const handleAp = async()=>{
 
-    // const handleAp = async()=>{
-        
-    // }
+  // }
+
+  const Api1 = [1, 2, 3, 4, 6, 7, 8, 9];
 
   return (
     <div>
-      
+      {Api.map((item) => {
+        return (
+          <>
+            <div>
+              <h1> {item.title}</h1>
+              <p>{item.description}</p>
+            </div>
+          </>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Api
+export default Api;
