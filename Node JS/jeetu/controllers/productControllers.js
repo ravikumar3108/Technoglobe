d1fecead9c7b507c8b7d9b1618fcb11a43b95198
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-06-19 12:26:19 +0530

    image

--- Node JS/jeetu/controllers/productControllers.js
+++ Node JS/jeetu/controllers/productControllers.js
@@ -1,21 +1,24 @@
 const Product = require("../models/porductModel")
-
+const fs = require('fs')
 
 const createProducts = async (req, res) => {
-    console.log(req.body)
     console.log(req.file)
 
-    // const product = await Product({
-    //     title: req.body.title,
-    //     des: req.body.des,
-    //     rating: req.body.rating,
-    //     price: req.body.price
-    // })
-    // await product.save()
-    // res.json({
-    //     mesage: product,
-    //     status: true
-    // })
+    const product = await Product({
+        title: req.body.title,
+        des: req.body.des,
+        image: {
+            data: fs.readFileSync('images/' + req.file.filename),
+            contentType: 'images/',
+        },
+        rating: req.body.rating,
+        price: req.body.price
+    })
+    await product.save()
+    res.json({
+        mesage: product,
+        status: true
+    })
 
 }
 
