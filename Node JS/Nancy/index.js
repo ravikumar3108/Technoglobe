b866b27b8b186cc771f8ef088f8977307a2ee019
Ravikumar3108 <raviluhaniwal318@gmail.com>
2024-07-03 11:00:39 +0530

    mongo

--- Node JS/Nancy/index.js
+++ Node JS/Nancy/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose');
 
 // middlewares
 app.use(express.json())
+main().catch(err => console.log(err));
 
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/nancy');
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+});
+
+const user = mongoose.model("users", userSchema)
 //  Api's 
 // 1. get  
+
 
 app.get("", (req, res) => {
     res.json({
@@ -16,16 +31,22 @@ app.get("", (req, res) => {
 })
 
 
-app.post("/post", (req, res) => {
+app.post("/post", async (req, res) => {
     console.log(req.body)
+    const { name, email, password } = req.body
+    const newUser = await user({
+        name: name,
+        email: email,
+        password: password,
+    })
+    const saveUser = await newUser.save()
     res.json({
         message: "post",
-        status: 200
+        status: 200,
+        data: saveUser
     })
     // res.send("Heloo")
 })
-
-
 
 app.listen(8080, () => {
     console.log("server create")
